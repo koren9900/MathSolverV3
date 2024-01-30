87bff0956aa90b1beecc9b2101c3835b204e2fbb
koren9900 <koren9900@gmail.com>
2024-01-31 00:14:58 +0200

    Functionality added: 0
    Functionality to add: 0
    
    bugs fixed:
    
    bugs to fix: 0
    
    Notes: changed from @Test to @ParameterizedTest

--- src/main/java/org/math/solver/Main.java
+++ src/main/java/org/math/solver/Main.java
@@ -1,10 +1,8 @@
 package org.math.solver;
 
 public class Main {
-
-
     public static void main(String[] args) {
-        String exp = "max(8,7";
+        String exp = "(5,3,7)+3*(4,7,-1)";
         System.out.println(new MathProblemSolver(exp).solve());
 
     }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -9,22 +9,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 public class MainTest {
 
-    @Test
-    public void testLiteralBinary(){
-        // check literals work correctly
-        assertEquals(new MathProblemSolver("1").solve(), "1=1");
-        // check addition works with literals
-        assertEquals(new MathProblemSolver("1+1").solve(), "1+1=2");
-        // check subtraction works with literals
-        assertEquals(new MathProblemSolver("1-1").solve(), "1-1=0");
-        // check multiplication works with literals
-        assertEquals(new MathProblemSolver("2*3").solve(), "2*3=6");
-        // check division works with literals
-        assertEquals(new MathProblemSolver("100/10").solve(), "100/10=10");
-        // check exponents work
-        assertEquals(new MathProblemSolver("2^5").solve(), "2^5=32");
-        assertEquals(new MathProblemSolver("121^0.5").solve(), "121^0.5=11");
-        assertEquals(new MathProblemSolver("121+4^0.5-4").solve(), "121+4^0.5-4=119");
+    @ParameterizedTest
+    @CsvSource(textBlock= """
+            # Input,        Expected
+            # check literals work correctly
+            1,              1=1
+            # check addition works with literals
+            1+1,            1+1=2
+            # check subtraction works with literals
+            1-1,            1-1=0
+            # check multiplication works with literals
+            2*3,            2*3=6
+            # check division works with literals
+            100/10,         100/10=10
+            # check exponents work
+            2^5,            2^5=32
+            121^0.5,        121^0.5=11
+            # additional checks
+            121+4^0.5-4,    121+4^0.5-4=119
+            """)
+    public void testLiteralBinary(String input, String expected){
+        assertEquals(new MathProblemSolver(input).solve(), expected);
     }
     @ParameterizedTest
     @CsvSource(textBlock= """
@@ -38,27 +43,32 @@ public class MainTest {
         // check factorial works
         assertEquals(new MathProblemSolver(input).solve(), expected);
     }
-    @Test
-    public void testLiteralNegation(){
-        // check single literal
-        assertEquals(new MathProblemSolver("-5").solve(), "-5=-5");
-        // check with binary operations
-        assertEquals(new MathProblemSolver("-5+5").solve(), "-5+5=0");
-        assertEquals(new MathProblemSolver("-5+-5").solve(), "-5+-5=-10");
-        assertEquals(new MathProblemSolver("-10-5").solve(), "-10-5=-15");
-        assertEquals(new MathProblemSolver("-11*3").solve(), "-11*3=-33");
-        assertEquals(new MathProblemSolver("-12*-3").solve(), "-12*-3=36");
-        assertEquals(new MathProblemSolver("100/-10").solve(), "100/-10=-10");
-        assertEquals(new MathProblemSolver("-5/-0.5").solve(), "-5/-0.5=10");
+    @ParameterizedTest
+    @CsvSource(textBlock= """
+            # Input,        Expected
+            # check single literal
+              -5,           -5=-5
+            # check with binary operations
+              -5+5,         -5+5=0
+              -5+-5,        -5+-5=-10
+              -10-5,        -10-5=-15
+              -11*3,        -11*3=-33
+              -12*-3,       -12*-3=36
+              100/-10,      100/-10=-10
+              -5/-0.5,      -5/-0.5=10
+            """)
+
+    public void testLiteralNegation(String input, String expected){
+        assertEquals(new MathProblemSolver(input).solve(), expected);
     }
     @ParameterizedTest
     @CsvSource(textBlock= """
             # Input,    Expected
+            # check factorial works
               -5!,      -5!=-120
               -1!,      -1!=-1
             """)
     public void testLiteralFactorialNegation(String input, String expected){
-        // check factorial works
         assertEquals(new MathProblemSolver(input).solve(), expected);
     }
 
@@ -77,59 +87,65 @@ public class MainTest {
               (1+2)!+3,                 (1+2)!+3=9
             """)
     public void testGrouping(String input, String expected){
-        // check single literal
         assertEquals(new MathProblemSolver(input).solve(), expected);
     }
 
-    @Test
-    public void testLiteralFunctions(){
-        // check functions work
-        assertEquals(new MathProblemSolver("MAX(1,2,3,4,5)").solve(), "max(1,2,3,4,5)=5");
-        assertEquals(new MathProblemSolver("Min(100,50)").solve(), "min(100,50)=50");
-
-        assertEquals(new MathProblemSolver("log(10,100)").solve(), "log(10,100)=2");
-        assertEquals(new MathProblemSolver("sqrt(1024)").solve(), "sqrt(1024)=32");
-
-        assertEquals(new MathProblemSolver("ceil(10.100)").solve(), "ceil(10.1)=11");
-        assertEquals(new MathProblemSolver("floor(1.9024)").solve(), "floor(1.9024)=1");
-
-        assertEquals(new MathProblemSolver("round(1.5001)").solve(), "round(1.5001)=2");
-        assertEquals(new MathProblemSolver("abs(---6)").solve(), "abs(---6)=6");
-
-        // check function stacking
-        assertEquals(new MathProblemSolver("round(max(1.49,1.51))").solve(), "round(max(1.49,1.51))=2");
-        assertEquals(new MathProblemSolver("min(4,3,2,1,abs(-5), log(10,100))").solve(), "min(4,3,2,1,abs(-5),log(10,100))=1");
-        assertEquals(new MathProblemSolver("min(ceil(min(ceil(10.79),10.99)),12)").solve(), "min(ceil(min(ceil(10.79),10.99)),12)=11");
-
-        // check factorial on functions
-        assertEquals(new MathProblemSolver("max(5,root(36,2))!").solve(), "max(5,root(36,2))!=720");
-        assertEquals(new MathProblemSolver("log(abs(-2),abs(8))!!").solve(), "log(abs(-2),abs(8))!!=720");
-
-        //check negation
-        assertEquals(new MathProblemSolver("-max(2,3)+-abs(-3)").solve(), "-max(2,3)+-abs(-3)=-6");
-        assertEquals(new MathProblemSolver("-max(800,log(4,12))/-sqrt(64)").solve(), "-max(800,log(4,12))/-sqrt(64)=100");
-    }
-    @Test
-    public void testVectorBinary(){
-        // check vectors work correctly
-        assertEquals(new MathProblemSolver("(1,1)").solve(), "(1,1)=(1,1)");
-        // check addition works with vectors
-        assertEquals(new MathProblemSolver("(1,1)+(1,1)").solve(), "(1,1)+(1,1)=(2,2)");
-        // check subtraction works with vectors
-        assertEquals(new MathProblemSolver("(3,2)-(3,3)").solve(), "(3,2)-(3,3)=(0,-1)");
-        // check multiplication works with vectors
-        assertEquals(new MathProblemSolver("(3,2,1)*7").solve(), "(3,2,1)*7=(21,14,7)");
-        // check division works with vectors
-        assertEquals(new MathProblemSolver("(121,99,11,1100)/11").solve(), "(121,99,11,1100)/11=(11,9,1,100)");
-        // additional tests
-        assertEquals(new MathProblemSolver("(2,7,5,4,8)+5*(1,1,1,1,1)").solve(), "(2,7,5,4,8)+5*(1,1,1,1,1)=(7,12,10,9,13)");
+    @ParameterizedTest
+    @CsvSource(textBlock= """
+            # Input:                    Expected Error
+            # check functions work
+            MAX(1,2,3,4,5):                         max(1,2,3,4,5)=5
+            Min(100,50):                            min(100,50)=50
+            log(10,100):                            log(10,100)=2
+            sqrt(1024):                             sqrt(1024)=32
+            ceil(10.100):                           ceil(10.1)=11
+            floor(1.9024):                          floor(1.9024)=1
+            round(1.5001):                          round(1.5001)=2
+            abs(---6):                              abs(---6)=6
+            # check function stacking
+            round(max(1.49,1.51)):                  round(max(1.49,1.51))=2
+            min(4,3,2,1,abs(-5), log(10,100)):      min(4,3,2,1,abs(-5),log(10,100))=1
+            min(ceil(min(ceil(10.79),10.99)),12):   min(ceil(min(ceil(10.79),10.99)),12)=11
+            # check factorial on functions
+            max(5,root(36,2))!:                     max(5,root(36,2))!=720
+            log(abs(-2),abs(8))!!:                  log(abs(-2),abs(8))!!=720
+            # check negation
+            -max(2,3)+-abs(-3):                     -max(2,3)+-abs(-3)=-6
+            -max(800,log(4,12))/-sqrt(64):          -max(800,log(4,12))/-sqrt(64)=100
+            """, delimiter = ':')
+    public void testLiteralFunctions(String input, String expected){
+        assertEquals(new MathProblemSolver(input).solve(), expected);
     }
 
-    @Test
-    public void testVectorNegation(){
-        // check vectors negation
-        assertEquals(new MathProblemSolver("-(1,1)").solve(), "-(1,1)=(-1,-1)");
-        assertEquals(new MathProblemSolver("(5,3)+-(2,3)").solve(), "(5,3)+-(2,3)=(3,0)");
+    @ParameterizedTest
+    @CsvSource(textBlock= """
+            # Input:                    Expected Error
+            # check vectors work correctly
+            (1,1):                      (1,1)=(1,1)
+            # check addition works with vectors
+            (1,1)+(1,1):                (1,1)+(1,1)=(2,2)
+            # check subtraction works with vectors
+            (3,2)-(3,3):                (3,2)-(3,3)=(0,-1)
+            # check multiplication works with vectors
+            (3,2,1)*7:                  (3,2,1)*7=(21,14,7)
+            # check division works with vectors
+            (121,99,11,1100)/11:        (121,99,11,1100)/11=(11,9,1,100)
+            # additional tests
+            (2,7,5,4,8)+5*(1,1,1,1,1):  (2,7,5,4,8)+5*(1,1,1,1,1)=(7,12,10,9,13)
+            """, delimiter = ':')
+    public void testVectorBinary(String input, String expected){
+        assertEquals(new MathProblemSolver(input).solve(), expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(textBlock= """
+            # Input:             Expected Error
+            # check vectors negation
+            -(1,1):              -(1,1)=(-1,-1)
+            (5,3)+-(2,3):        (5,3)+-(2,3)=(3,0)
+            """, delimiter = ':')
+    public void testVectorNegation(String input, String expected){
+        assertEquals(new MathProblemSolver(input).solve(), expected);
     }
     @ParameterizedTest
     @CsvSource(textBlock= """
@@ -145,6 +161,7 @@ public class MainTest {
               max(min(3,4,3,3),fun(23)):        f is an unrecognised identifier
               8!+max7,8)-120:                   Expect '(' after a function
               2^min(4,7,max(-1,7):              Expect '(' in the end of a function
+              (4+7,5^3*max(2,2))+(1^2,2,3):     Can't operate between different size vectors
             """,delimiter = ':')
     public void testErrors(String input, String error){
       Exception e = assertThrows(RuntimeError.class, ()-> new MathProblemSolver(input).solve() );
